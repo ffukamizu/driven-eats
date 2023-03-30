3e3d10e0d6f6206b5c523e3e217d6d5347bdde78
ffukamizu <f.fukamizu@live.com>
2023-03-30 04:50:22 -0300

    Added selector functionality to menu

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function highlightItem () {
-    
+var isHighlighted = false;
+
+function highlightFood(id) {
+  if (isHighlighted === false) {
+    //highlight a product
+    document.getElementById(id).classList.add("highlight-item");
+    currentHighlight = id;
+    isHighlighted = true;
+  } else if (isHighlighted === true && currentHighlight != id) {
+    //highlight another product while unselecting the previous one 
+    document.getElementById(currentHighlight).classList.remove("highlight-item");
+    document.getElementById(id).classList.add("highlight-item");
+    currentHighlight = id;
+  } else {
+    //unselect a product that is already highlighted
+    document.getElementById(id).classList.remove("highlight-item");
+    isHighlighted = false;
+  }
 }
